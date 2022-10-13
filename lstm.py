82faae345d361e5ba5a4b7941991894261013bc3
yunfanlong <yunfanlong007@gmail.com>
2022-10-13 15:29:24 -0700

    Add VAE-LSTM

--- lstm.py
+++ lstm.py
@@ -68,3 +68,66 @@ def lstm(input_dim, timesteps):
 
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
     return model
+
+def lstm_vae(input_dim, timesteps, batch_size, intermediate_dim, latent_dim, epsilon_std=1.):
+
+    x = Input(shape=(timesteps, input_dim,))
+
+    # LSTM encoding
+    h = LSTM(intermediate_dim)(x)
+
+
+    # VAE Z layer
+    z_mean = Dense(latent_dim)(h)
+    z_log_sigma = Dense(latent_dim)(h)
+   
+    def sampling(args):
+        z_mean, z_log_sigma = args
+        epsilon = K.random_normal(shape=(batch_size, latent_dim),
+                                  mean=0., stddev=epsilon_std)
+        return z_mean + z_log_sigma * epsilon
+
+
+    z = Lambda(sampling)([z_mean, z_log_sigma])
+   
+    # decoded layer
+    decoder_h = LSTM(intermediate_dim, return_sequences=True)
+    decoder_mean = LSTM(input_dim, return_sequences=True)
+
+
+    h_decoded = RepeatVector(timesteps)(z)
+    h_decoded = decoder_h(h_decoded)
+
+
+    # decoded layer
+    x_decoded_mean = decoder_mean(h_decoded)
+   
+    # end-to-end autoencoder
+    vae = Model(x, x_decoded_mean)
+
+
+    # encoder, from inputs to latent space
+    encoder = Model(x, z_mean)
+
+
+    # generator, from latent space to reconstructed inputs
+    decoder_input = Input(shape=(latent_dim,))
+
+
+    _h_decoded = RepeatVector(timesteps)(decoder_input)
+    _h_decoded = decoder_h(_h_decoded)
+
+
+    _x_decoded_mean = decoder_mean(_h_decoded)
+    generator = Model(decoder_input, _x_decoded_mean)
+   
+    def vae_loss(x, x_decoded_mean):
+        xent_loss = objectives.mse(x, x_decoded_mean)
+        kl_loss = - 0.5 * K.mean(1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma))
+        loss = xent_loss + kl_loss
+        return loss
+
+
+    vae.compile(optimizer='rmsprop', loss=vae_loss)
+   
+    return vae, encoder, generator
