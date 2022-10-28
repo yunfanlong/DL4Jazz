64b5161a4e82b7e08753df320075ef8b99c4d4a7
yunfanlong <yunfanlong007@gmail.com>
2022-10-27 19:31:13 -0700

    Update to latest model - last commit missed several imports

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DL4Jazz
 Bi-LSTM, VAE-LSTM, and LSTM for generating Jazz music.
+
+# Model Architechture
+
--- lstm.py
+++ lstm.py
@@ -4,19 +4,20 @@ Project:    DL4Jazz
 Purpose:    LSTM Models for Music Generation
 '''
 
-from __future__ import print_function
-
 import keras
 from keras import backend as K
 from keras.models import Sequential, Model
-from keras.layers import Input, LSTM, RepeatVector
-from keras.layers.core import Dense, Activation, Dropout, Flatten, Lambda
+from keras.layers import Input, LSTM, RepeatVector, Bidirectional
+from keras.layers import Dense, Activation, Dropout, Flatten, Lambda
 from keras.optimizers import SGD, RMSprop, Adam
+from keras.regularizers import l1, l2
 from keras import objectives
+from keras.utils import plot_model
 import numpy as np
+import matplotlib.pyplot as plt
 
 ''' Build a 2-layer LSTM from a training corpus '''
-def build_model(corpus, val_indices, max_len, N_epochs=128, model=None):
+def build_model(corpus, val_indices, max_len, N_epochs=128, model_choice=None):
     # number of different values or words in corpus
     N_values = len(set(corpus))
 
@@ -37,20 +38,31 @@ def build_model(corpus, val_indices, max_len, N_epochs=128, model=None):
             X[i, t, val_indices[val]] = 1
         y[i, val_indices[next_values[i]]] = 1
     
-    if model=='lstm-vae':
-        model, _, _ = lstm_vae(input_dim=N_values, 
-                               timesteps=max_len, 
-                               batch_size=N_epochs, 
-                               intermediate_dim=128, 
-                               latent_dim=64, 
-                               epsilon_std=1.) 
     
-    elif model=='lstm':
+    if model_choice=='lstm':
         # build a 2 stacked LSTM
         # default
         model = lstm(input_dim=N_values, timesteps=max_len)
+        plot_model(model, to_file='lstm.png')
+        history = model.fit(X, y, batch_size=128, epochs=N_epochs)
+    
+    elif model_choice=='vae-lstm':
+        model, _, _ = vae_lstm(input_dim=N_values, 
+                               timesteps=max_len, 
+                               batch_size=N_epochs,
+                               intermediate_dim=128, 
+                               latent_dim=64, 
+                               epsilon_std=0.5) 
+        plot_model(model, to_file='vae-lstm.png')
+        history = model.fit(X, X, batch_size=128, epochs=N_epochs)
 
-    model.fit(X, y, batch_size=128, epochs=N_epochs)
+    elif model_choice=='bi-lstm':
+        model = bi_lstm(input_dim=N_values, timesteps=max_len)
+        plot_model(model, to_file='bi-lstm.png')
+        history = model.fit(X, y, batch_size=128, epochs=N_epochs)
+
+    else:
+        raise Exception
 
     return model
 
@@ -74,25 +86,25 @@ def bi_lstm(input_dim, timesteps):
     model = Sequential() #1
     model.add(Bidirectional(LSTM(128, return_sequences=True, input_shape=(timesteps, input_dim))))
     model.add(Dropout(0.2))
-    #2
+
     model.add(Bidirectional(LSTM(610, return_sequences=True)))
     model.add(Dropout(0.2))
-    #3
+
     model.add(Bidirectional(LSTM(610, return_sequences=True)))
     model.add(Dropout(0.2))
-    #4
+
     model.add(Bidirectional(LSTM(610, return_sequences=True)))
     model.add(Dropout(0.2))
-    #4
+
     model.add(Bidirectional(LSTM(610, return_sequences=True)))
     model.add(Dropout(0.2))
-    #5
+
     model.add(Bidirectional(LSTM(610, return_sequences=True)))
     model.add(Dropout(0.2))
-    #6
+
     model.add(Bidirectional(LSTM(610, return_sequences=True)))
     model.add(Dropout(0.2))
-    #7
+
     model.add(Bidirectional(LSTM(128, return_sequences=False)))
     model.add(Dropout(0.2))
 
@@ -100,67 +112,78 @@ def bi_lstm(input_dim, timesteps):
     model.add(Activation('softmax'))
 
     model.compile(loss='categorical_crossentropy', optimizer='adam', )
+    # model.fit(X, y, batch_size=128, nb_epoch=N_epochs)
     return model
+    
 
-def lstm_vae(input_dim, timesteps, batch_size, intermediate_dim, latent_dim, epsilon_std=1.):
+def vae_lstm(input_dim, timesteps, batch_size, intermediate_dim, latent_dim, epsilon_std=1.):
 
-    x = Input(shape=(timesteps, input_dim,))
+    """
+    Creates an LSTM Variational Autoencoder (VAE). Returns VAE, Encoder, Generator. 
+    # Arguments
+        input_dim: int.
+        timesteps: int, input timestep dimension.
+        batch_size: int.
+        intermediate_dim: int, output shape of LSTM. 
+        latent_dim: int, latent z-layer shape. 
+        epsilon_std: float, z-layer sigma.
+    # References
+        - [Building Autoencoders in Keras](https://blog.keras.io/building-autoencoders-in-keras.html)
+        - [Generating sentences from a continuous space](https://arxiv.org/abs/1511.06349)
+    """
+
+    x = Input(shape=(timesteps, input_dim, ))
 
     # LSTM encoding
-    h = LSTM(intermediate_dim)(x)
-
+    h = LSTM(intermediate_dim, return_sequences=True)(x)
 
     # VAE Z layer
     z_mean = Dense(latent_dim)(h)
     z_log_sigma = Dense(latent_dim)(h)
-   
+    print('This is mu:', K.int_shape(z_mean), 'latent_dim', latent_dim)
+    
     def sampling(args):
         z_mean, z_log_sigma = args
-        epsilon = K.random_normal(shape=(batch_size, latent_dim),
+        epsilon = K.random_normal(shape=(timesteps, latent_dim, ),
                                   mean=0., stddev=epsilon_std)
-        return z_mean + z_log_sigma * epsilon
+        z = K.mean(z_mean + z_log_sigma * epsilon, axis=1)
+        return z
 
-
-    z = Lambda(sampling)([z_mean, z_log_sigma])
-   
-    # decoded layer
+    z = Lambda(sampling, output_shape=(latent_dim, ))([z_mean, z_log_sigma])
+    
+    # decoded LSTM layer
     decoder_h = LSTM(intermediate_dim, return_sequences=True)
     decoder_mean = LSTM(input_dim, return_sequences=True)
-
 
     h_decoded = RepeatVector(timesteps)(z)
     h_decoded = decoder_h(h_decoded)
 
-
     # decoded layer
     x_decoded_mean = decoder_mean(h_decoded)
-   
+    
     # end-to-end autoencoder
     vae = Model(x, x_decoded_mean)
 
-
     # encoder, from inputs to latent space
     encoder = Model(x, z_mean)
-
+    encoder.summary()
 
     # generator, from latent space to reconstructed inputs
     decoder_input = Input(shape=(latent_dim,))
 
-
     _h_decoded = RepeatVector(timesteps)(decoder_input)
     _h_decoded = decoder_h(_h_decoded)
 
-
     _x_decoded_mean = decoder_mean(_h_decoded)
     generator = Model(decoder_input, _x_decoded_mean)
-   
+    generator.summary()
+    
     def vae_loss(x, x_decoded_mean):
         xent_loss = objectives.mse(x, x_decoded_mean)
-        kl_loss = - 0.5 * K.mean(1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma))
+        kl_loss = - 0.5 * K.mean(1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma), axis=-1)
         loss = xent_loss + kl_loss
         return loss
 
-
-    vae.compile(optimizer='rmsprop', loss=vae_loss)
-   
+    vae.compile(optimizer='adam', loss=vae_loss)
+    
     return vae, encoder, generator
